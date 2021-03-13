9479d5b30a1ea6f26ae02a84bb2e93eff41c78b1
Chris <programmeraccnt@gmail.com>
2021-03-13 11:45:45 -0500

    Add support for model page

--- router.js
+++ router.js
@@ -3,8 +3,10 @@ const fileHandler = require("./file-handler.js")
 
 exports.route = (request, response) => {
   const pathname = url.parse(request.url).pathname
-
   switch (pathname) {
+    case '/model':
+      fileHandler.readFile(request, response, "./public/model/model.html")
+      break
     default:
       fileHandler.readFile(request, response, "./public/home/index.html")
       break
